230e1903945704afb610be9a1c13a5c8623f1994
LiuTao <369069118@qq.com>
2019-02-22 10:37:59 +0800

    1

---
+++ githubtest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  githubtest
+//
+//  Created by liutao on 2019/2/22.
+//  Copyright © 2019 liutao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ githubtest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  githubtest
+//
+//  Created by liutao on 2019/2/22.
+//  Copyright © 2019 liutao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
